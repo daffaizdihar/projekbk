f1aa1c7236537f847e0b1d73a9234d4125a3ecf5
Daffa Izdihar <86041236+daffaizdihar@users.noreply.github.com>
2024-10-21 10:14:46 +0800

    upload main

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+
+    <title>bksmansabpp</title>
+  </head>
+<body>
+   <!-- Jumbotron -->
+   <div class="jumbotron container-fluid" style="padding: 2rem 1rem; margin-bottom: 2rem; background-color: #e9ecef; border-radius: 0.3rem">
+      <div class="text-center">
+         <img style="width:10%; height:10%;" src="logosmansa.png" alt="Photo" />
+         <h1 class="text-center">BK SMANSA BPP</h1>
+         <p>Sistem Bimbingan Konseling SMA Negeri 1 Balikpapan</p>
+      </div>
+   </div>
+   <!-- Navbar -->
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top" style="margin-top: -30px">
+      <div class="container-fluid">
+        <a class="navbar-brand text-uppercase" href="https://sman1-bpp.sch.id/" target="_blank">SMANSA</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#" target="_blank">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../login" target="_blank">Register</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://github.com/daffaizdihar" target="_blank">Products</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+</body>
+</html>
